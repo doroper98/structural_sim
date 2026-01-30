a26007f22b93a038a4a0cb40ca4d3cdd3591f312
doroper98 <monosound09@gmail.com>
2026-01-30 20:02:41 +0900

    Initial commit: structural simulation project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Structural Simulation Project
+
+이 프로젝트는 구조 시뮬레이션을 위한 코드입니다.
+
+## 기능
+- 메싱 모듈
+- 구조 해석
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Structural Simulation Started" << std::endl;
+    return 0;
+}
